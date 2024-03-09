08ddd590e7ddb69414bf846353870558c5be68a6
Alexander <ndunda.alex@gmail.com>
2024-03-10 02:27:55 +0300

    resolve conflict

--- pages/2_📚_Data.py
+++ pages/2_📚_Data.py
@@ -14,7 +14,6 @@ st.set_page_config(
 def data_page():
     login_form()
     if is_authenticated():
-
         data = pd.DataFrame(pd.read_excel("Dataset\Lp2_df_coc.xlsx"))
 
         st.title("Explore Customer Data ⭐")
